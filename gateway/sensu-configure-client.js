9739962767d94c5e23784f7337db422a09166792
Brad Campbell <bradjc5@gmail.com>
2016-04-17 15:19:15 -0400

    don't bother setting address
    it could change
    
    hard to ensure networking is started first
    
    use our ip check to get ip addresses

--- gateway/sensu-configure-client.js
+++ gateway/sensu-configure-client.js
@@ -9,28 +9,12 @@ var CONFIG_FILENAME   = '/etc/sensu/conf.d/client.json';
 
 var output = JSON.parse(fs.readFileSync(CONFIG_FILENAME));
 
-// Get IP address
-function get_first_ip_address () {
-	var os = require('os');
-	var ifaces = os.networkInterfaces();
-
-	console.log(ifaces);
-
-	for (var ifname in ifaces) {
-		if (ifname != lo) {
-			return ifaces[ifname][0].address;
-		}
-	}
-	return '';
-}
-
 if ('client' in output) {
 	getmac.getMac(function (err, addr) {
 		var macaddr = adddr.replace(/[^A-Z0-9-]/ig, '');
 		var name = 'swarm-gateway-' + macaddr;
 
 		output['client']['name'] = name;
-		output['client']['address'] = get_first_ip_address();
 
 		fs.writeFileSync(CONFIG_FILENAME, JSON.stringify(output));
 
