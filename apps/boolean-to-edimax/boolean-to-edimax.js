1e2d618b7f615294a9390eacdb6dce98d1a1cb50
Pat Pannuto <pat.pannuto@gmail.com>
2016-07-10 14:05:29 -0400

    Add 30s warning before turning lights off

--- apps/boolean-to-edimax/boolean-to-edimax.js
+++ apps/boolean-to-edimax/boolean-to-edimax.js
@@ -20,6 +20,44 @@ if ( ! ('controlInvert' in conf) ) {
 console.log("connecting to " + conf.mqttBroker);
 var client = mqtt.connect(conf.mqttBroker);
 
+
+
+function setEdimaxesPowerState (onoff, edimaxes=null) {
+	if ( edimaxes == null ) {
+		edimaxes = conf.edimaxes;
+	}
+
+	for (var i=0; i<edimaxes.length; i++) {
+		edimax.setSwitchState(onoff, edimaxes[i]).catch(function(e) {console.log(e)});
+	}
+}
+
+
+var warningTimeout = null;
+function warnLightsOff (count=0) {
+	// Blink a light three times and then wait 30s before actually turning off
+	// the lights. Calling clearTimeout(warningTimeout) will abort the shutoff.
+	// The warning light will remain off during the 30s warning interval, callers
+	// that abort shutoff should power it back on to notify of the cancellation
+	var light = conf.edimaxes.slice(0,1);
+
+	if ((count % 2) == 0) {
+		setEdimaxesPowerState(false, light);
+		warningTimeout = setTimeout(function () {
+			warnLightsOff(count + 1);
+		}, 1000*1);
+	} else if (count < 7) {
+		setEdimaxesPowerState(true, light);
+		warningTimeout = setTimeout(function () {
+			warnLightsOff(count + 1);
+		}, 1000*1);
+	} else {
+		warningTimeout = setTimeout(function () {
+			setEdimaxesPowerState(false);
+		}, 1000*30);
+	}
+}
+
 client.on('connect', function () {
 	console.log('connected');
 
@@ -40,8 +78,17 @@ client.on('connect', function () {
 			onoff = !onoff;
 		}
 
-		for (var i=0; i<conf.edimaxes.length; i++) {
-			edimax.setSwitchState(onoff, conf.edimaxes[i]).catch(function(e) {console.log(e)});
+		if (onoff) {
+			// Turn on immediately
+			setEdimaxesPowerState(onoff);
+
+			// Cancel any pending off event
+			if ( warningTimeout != null ) {
+				clearTimeout(warningTimeout);
+				warningTimeout = null;
+			}
+		} else {
+			warnLightsOff();
 		}
 	});
 });
