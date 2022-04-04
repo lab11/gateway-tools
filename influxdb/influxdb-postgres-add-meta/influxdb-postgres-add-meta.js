9a40a0a2e94ac95d960276f1f23e1ee97e8b95c3
Brad Campbell <bradjc5@gmail.com>
2022-04-04 17:56:34 -0400

    check for device_id as last tag

--- influxdb/influxdb-postgres-add-meta/influxdb-postgres-add-meta.js
+++ influxdb/influxdb-postgres-add-meta/influxdb-postgres-add-meta.js
@@ -86,6 +86,10 @@ async function start() {
 				var device_id_index = pieces[0].indexOf('device_id');
 				if (device_id_index > -1) {
 					var end_index = pieces[0].indexOf(',', device_id_index);
+					if (end_index == -1) {
+						// device_id was the last tag, so use the length of pieces[0]
+						end_index = pieces[0].length;
+					}
 					var device_id = pieces[0].slice(device_id_index+10, end_index);
 
 					// Lookup if we know anything about that device
